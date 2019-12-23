f3823e495b50618d8a596459c321c40b3cbf07f8
root <root@ip-172-31-18-151.ap-southeast-1.compute.internal>
2019-12-23 13:49:14 +0000

    nives.com

---
+++ src/main/java/com/nives/App.java
@@ -0,0 +1,13 @@
+package com.nives;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
